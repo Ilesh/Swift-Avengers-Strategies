81dbd307cebb316def843bb0494dccaab4e339de
manish <manish@siliconithub.com>
2018-08-29 11:54:33 +0530

    - Navigation push animation.

--- SAS/Entities/Extensions/UIKit/UINavigationControllerExtensions.swift
+++ SAS/Entities/Extensions/UIKit/UINavigationControllerExtensions.swift
@@ -90,13 +90,25 @@ public extension UINavigationController {
         }
     }
     
-    public func pushViewControllerFromBottom(_ viewController: UIViewController, anmatio: Bool) {
+    // Push View Controller from Bottom like present view controller
+    public func pushViewControllerFromBottom(_ viewController: UIViewController, animation: Bool) {
+        let transition:CATransition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromTop
+        self.navigationController!.view.layer.add(transition, forKey: kCATransition)
+        self.navigationController?.pushViewController(viewController, animated: animation)
+    }
+    
+    // Push View Controller from Top like present view controller
+    public func pushViewControllerFromTop(_ viewController: UIViewController, animation: Bool) {
         let transition:CATransition = CATransition()
         transition.duration = 0.5
         transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         transition.type = kCATransitionPush
         transition.subtype = kCATransitionFromBottom
         self.navigationController!.view.layer.add(transition, forKey: kCATransition)
-        self.navigationController?.pushViewController(viewController, animated: false)
+        self.navigationController?.pushViewController(viewController, animated: animation)
     }
 }
